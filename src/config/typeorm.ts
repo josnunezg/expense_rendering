5a62dec05e19582f1d657d12c14e6daa685d72b5
Jose Nuñez <josnun@MacBook-Pro-de-Jose.local>
2021-03-25 01:48:01 -0300

    entities

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -4,27 +4,20 @@ import path from 'path';
 const logging =
   process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'text'
 
-const path_entities =
-  process.env.NODE_ENV == 'development' ? '../entities/**/**.ts' : '../entities/**/**.js';
-
-const paths = path.join(__dirname, path_entities);
-
 async function connect() {
-  console.log(paths);
   await createConnection({
     type: 'postgres',
-    // host: process.env.DATABASE_HOST,
-    // port: 5432,
-    // username: process.env.DATABASE_USER,
-    // password: process.env.DATABASE_PASSWORD,
-    // database: process.env.DATABASE_NAME,
-    url: process.env.DATABASE_URL,
-    entities: [
-      paths
-    ],
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [path.join(__dirname, '../entities/**/**.entity{.ts,.js}')],
     synchronize: process.env.NODE_ENV == 'development',
     logging,
-    ssl: process.env.NODE_ENV == 'production'
+    ssl: process.env.NODE_ENV == 'production' && {
+      rejectUnauthorized: false
+    }
   });
   console.log('Database connected');
 }
