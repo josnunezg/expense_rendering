1ee05b18680a74f0746982c8688de36887935ce5
Jose Nuñez <josnun@MacBook-Pro-de-Jose.local>
2021-03-25 02:01:55 -0300

    fix options

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,24 +1,41 @@
-import { createConnection } from 'typeorm';
+import { createConnection, ConnectionOptions } from 'typeorm';
 import path from 'path';
 
 const logging =
   process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'text'
 
-async function connect() {
-  await createConnection({
+
+function createOption(): ConnectionOptions {
+  const opions: ConnectionOptions = {
     type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: 5432,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    entities: [path.join(__dirname, '../entities/**/**.entity{.ts,.js}')],
-    synchronize: process.env.NODE_ENV == 'development',
-    logging,
-    ssl: process.env.NODE_ENV == 'production' && {
+    entities: [path.join(__dirname, '../entities/**/**.entity{.ts,.js}')]
+  }
+
+  if (process.env.NODE_ENV == 'development') {
+    return {
+      ...opions,
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: true,
+      logging: true
+    }
+  }
+
+  return {
+    ...opions,
+    url: process.env.DATABASE_URL,
+    ssl: {
       rejectUnauthorized: false
     }
-  });
+  }
+}
+
+async function connect() {
+  const options = createOption();
+  await createConnection(options);
   console.log('Database connected');
 }
 
