ad261757cdf06dd40ee6fd09d721ce781c43c3cf
Jose Nuñez <josnun@MacBook-Pro-de-Jose.local>
2021-03-25 01:05:40 -0300

    connect with url

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -7,11 +7,12 @@ const logging =
 async function connect() {
   await createConnection({
     type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: 5432,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    // host: process.env.DATABASE_HOST,
+    // port: 5432,
+    // username: process.env.DATABASE_USER,
+    // password: process.env.DATABASE_PASSWORD,
+    // database: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL,
     entities: [
       path.join(__dirname, '../entities/**/**.ts')
     ],
