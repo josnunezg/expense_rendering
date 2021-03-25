fad5160cb02d7fbf5a3f0ebcd951484977686e96
Jose Nuñez <josnun@MacBook-Pro-de-Jose.local>
2021-03-25 00:46:46 -0300

    add host

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -7,7 +7,7 @@ const logging =
 async function connect() {
   await createConnection({
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.DATABASE_HOST,
     port: 5432,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
